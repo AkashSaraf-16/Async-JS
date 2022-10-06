39857f5e17f21241927cfe793757d3fc4f4723d3
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2022-10-06 13:36:02 +0530

    Async Callback

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 // Import stylesheets
 import './style.css';
 
-// Synchronous JavaScript
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Async JavaScript</h1>`;
 
-function sayHi(name) {
-  console.log(`Hi , ${name}!!!`);
-}
+// Synchronous JavaScript
+// function sayHi(name) {
+//   console.log(`Hi , ${name}!!!`);
+// }
 
-function processUser(callback) {
-  callback('Akash');
+// function processUser(callback) {
+//   callback('Akash');
+// }
+// processUser(sayHi);
+
+// Asynchronous JavaScript
+function getUser(x, callback) {
+  setTimeout(() => {
+    console.log('got it');
+    callback(`${x}@${Math.floor(Math.random() * 1000000000)}`);
+  }, 5000);
 }
-processUser(sayHi);
+function getPhoneNo(z, callback) {
+  setTimeout(() => {
+    callback(z);
+  }, 5000);
+}
+let name = 'Akash';
+getUser(name, (x) => {
+  console.log(`We got the user, ${x}`);
+  getPhoneNo(x, (y) => {
+    let data = y.split('@');
+    let phone = data[1];
+    console.log(`Contact details for ${name} is ${phone}`);
+  });
+});
